c6255d2442e31eee513a8b7e17537d3c97cbd338
Simon Scabello <simon.scabello@live.com>
2021-05-24 18:50:42 -0300

    Finaliza os cruds de membros e eventos. Instala o sweetalert.

--- resources/views/members/create.blade.php
+++ resources/views/members/create.blade.php
@@ -5,19 +5,22 @@
     <div class="card-header">
         <h2 class="card-title">Novo membro</h2>
     </div>
-    <!-- /.card-header -->
-    <!-- form start -->
     <form method="POST" action="{{route('members.store')}}">
         @csrf
         <div class="card-body">
             <div class="form-group">
-                <label for="nome">Nome</label>
-                <input type="text" name="nome" class="form-control" required>
+                <label for="name">Nome</label>
+                <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" required value="{{old('name')}}">
+                @error('name')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+                @enderror
             </div>
 
             <div class="form-group">
-                <label for="sexo">Sexo</label>
-                <select name="sexo" class="form-control" required>
+                <label for="gender">Sexo</label>
+                <select name="gender" class="form-control" required>
                     <option value="Masculino">Masculino</option>
                     <option value="Feminino">Feminino</option>
                 </select>
@@ -25,29 +28,55 @@
 
             <div class="form-group">
                 <label for="cpf">CPF</label>
-                <input type="text" name="cpf" class="form-control" required>
+                <input type="text" name="cpf" class="form-control @error('cpf') is-invalid @enderror" required value="{{old('cpf')}}">
+                @error('cpf')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{old('email')}}">
+                @error('email')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="age">Data de nascimento</label>
+                <input type="date" class="form-control @error('age') is-invalid @enderror" name="age" value="{{old('age')}}">
+                @error('age')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+                @enderror
             </div>
             <div class="form-group">
-                <label for="email">Email address</label>
-                <input type="email" name="email" class="form-control">
+                <label for="phone">Telefone</label>
+                <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror" value="{{old('phone')}}">
+                @error('phone')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+                @enderror
             </div>
             <div class="form-group">
-                <label for="data_de_nascimento">Data de nascimento</label>
-                <input type="date" class="form-control" name="data_de_nascimento">
-            </div>
-            <div class="form-group">
-                <label for="telefone">Telefone</label>
-                <input type="text" name="telefone" class="form-control">
-            </div>
-            <div class="form-group">
-                <label for="endereco">Email address</label>
-                <input type="text" class="form-control" name="endereco">
+                <label for="address">Endereço</label>
+                <input type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{old('address')}}">
+                @error('address')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+                @enderror
             </div>
         </div>
-        <!-- /.card-body -->
 
         <div class="card-footer">
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
         </div>
     </form>
 </div>
